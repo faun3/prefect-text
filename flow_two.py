47788571ff1145112168e7e70c9a1f426728d618
antonio666 <claudius.c@index.dev>
2024-09-06 15:04:59 +0300

    fix: better params

--- flow_two.py
+++ flow_two.py
@@ -6,8 +6,9 @@ from datetime import timedelta
 
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
-def get_db_connection(connection_string: str):
-    engine = create_engine(connection_string)
+def get_db_connection():
+    conn_string = Secret.load("etl-staging-db-connection-string")
+    engine = create_engine(conn_string)
     return engine
 
 
@@ -20,8 +21,7 @@ def fetch_latest_10_rows_from_table(engine: Engine, table_name: str, order_by_co
 
 
 @flow(log_prints=True)
-def print_first_10_rows(connection_string: str, table_name: str, order_by_col: str):
-    engine = get_db_connection(connection_string)
+def print_first_10_rows(engine: Engine, table_name: str, order_by_col: str):
     rows = fetch_latest_10_rows_from_table(engine, table_name, order_by_col)
 
     print(f"First 10 rows from {table_name} ordered by {order_by_col}:")
@@ -30,8 +30,8 @@ def print_first_10_rows(connection_string: str, table_name: str, order_by_col: s
 
 
 if __name__ == "__main__":
-    connection_string = Secret.load("etl-staging-db-connection-string")
+    engine = get_db_connection()
     table_name = "qualifiedjob"
     order_by_col = "processed_date"
 
-    print_first_10_rows(connection_string, table_name, order_by_col)
+    print_first_10_rows(engine, table_name, order_by_col)
